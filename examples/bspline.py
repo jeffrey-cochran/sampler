92b152fdb5a862a57a85daa3c7fd0db2776ca01c
Jeffrey Cochran <koenigcochran@gmail.com>
2024-12-16 17:29:04 -0500

    confirm bspline weight arrangement

--- examples/bspline.py
+++ examples/bspline.py
@@ -1,2 +1,25 @@
 
-import sampler.utils.bsplines.bspline
+import sampler.utils.bsplines.bspline as bspline
+import sampler.utils.bsplines.tensor_basis as tensor_basis
+import sampler.utils.bsplines.tensor_grid as tensor_grid
+import sampler.utils.bsplines.functions as functions
+import torch
+
+x_basis = bspline.BSpline.uniform(
+    lims=(0,1),
+    n_segments=10,
+    degree=3,
+    dtype = torch.float64
+)
+
+xy_basis = tensor_basis.TensorBasis(x_basis, x_basis)
+
+xy_grid = tensor_grid.TensorGrid(
+    xs = torch.linspace(0,1,100)
+)
+
+weights = torch.randn(100,1, dtype=torch.float64)
+
+# TODO: confirm the orderign of the weights
+f = functions.Functions(xy_basis, weights)
+
--- src/sampler/utils/bsplines/tensor_basis.py
+++ src/sampler/utils/bsplines/tensor_basis.py
@@ -5,6 +5,7 @@ from torch import nn
 from math import log
 
 # Local
+from sampler.utils.bsplines.bspline import BSpline
 from sampler.utils.bsplines.tensor_grid import TensorGrid
 
 
@@ -13,37 +14,55 @@ class TensorBasis(nn.Module):
     dim = 2
 
 
-    def __init__(self, f_x, f_y=None):
+    def __init__(self, f_x:BSpline, f_y:BSpline=None):
         super().__init__()
         self.f_x = f_x
         self.f_y = f_y
 
 
-    def forward(self, points):
+    def forward(self, points:torch.Tensor|TensorGrid) -> torch.Tensor:
+
         f_x = self.f_x
         f_y = self.f_y or f_x
+
+        out_eval = None
+
         if isinstance(points, TensorGrid):
+            # Apparently it's easier to compute on this
+            # user-defined grid than on the x,y coords
             f = f_x(points.xs)
+
             if self.f_y is None and points.ys is None:
+                # This implies that the basis and points
+                # are the same in both x and y
                 g = f
             else:
+                # Evaluate the basis in the y direction
                 ys = points.ys if points.ys is not None else points.xs
                 g = f_y(ys)
-            return self.tensor_product(f, g, points.x_varies_first)
+
+            out_eval = self.tensor_product(f, g, points.x_varies_first)
         else:
             X = points[:, 0]
             Y = points[:, 1]
             f = f_x(X)
             g = f_y(Y)
-            return self.outer_product(f, g)
+            out_eval = self.outer_product(f, g)
 
+        return out_eval
 
     def gradient(self, points):
+
+        out_grad_eval = None
+
         f_x = self.f_x
         f_y = self.f_y or f_x
+
         if isinstance(points, TensorGrid):
+
             f = f_x(points.xs)
             df = f_x.derivative(points.xs)
+
             if self.f_y is None and points.ys is None:
                 g = f
                 dg = df
@@ -51,7 +70,8 @@ class TensorBasis(nn.Module):
                 ys = points.ys if points.ys is not None else points.xs
                 g = f_y(ys)
                 dg = f_y.derivative(ys)
-            return torch.stack(
+
+            out_grad_eval = torch.stack(
                 (
                     self.tensor_product(df, g, points.x_varies_first),
                     self.tensor_product(f, dg, points.x_varies_first),
@@ -61,13 +81,18 @@ class TensorBasis(nn.Module):
         else:
             X = points[:, 0]
             Y = points[:, 1]
+
             f = f_x(X)
             g = f_y(Y)
+
             df = f_x.derivative(X)
             dg = f_y.derivative(Y)
-            return torch.stack(
+
+            out_grad_eval = torch.stack(
                 (self.outer_product(df, g), self.outer_product(f, dg)), -1
             )
+
+        return out_grad_eval
 
 
     def hessian(self, points):
@@ -112,23 +137,33 @@ class TensorBasis(nn.Module):
 
 
     @staticmethod
-    def outer_product(a, b):
+    def outer_product(a:torch.Tensor, b:torch.Tensor) -> torch.Tensor:
         # a should be n × f
         # b should be n × g
         # output should then be n × (f*g)
         n = a.shape[0]
         f = a.shape[1]
         g = b.shape[1]
-        return (a.reshape(n, f, 1) * b.reshape(n, 1, g)).reshape(n, f * g)
+
+        # TODO: confirm the ordering of the reshaped tensor
+        return (
+            a.reshape(n, f, 1) * b.reshape(n, 1, g)
+        ).reshape(n, f * g)
 
 
     @staticmethod
-    def tensor_product(a, b, x_varies_first):
+    def tensor_product(
+        a:torch.Tensor,
+        b:torch.Tensor,
+        x_varies_first:bool
+    ) -> torch.Tensor:
         # a should be m × f
         # b should be n × g
         # output should then be mn × fg
+
         m, f = a.shape
         n, g = b.shape
+
         if x_varies_first:
             # when reshaping: last index varies first. We therefore now want the last indices to be m, and g
             a = a.reshape(1, m, f, 1)
